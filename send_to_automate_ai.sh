6ee5c6a65daaa9e9508a191572a4dbc7e1aeb086
saihgupr <saihgupr@duck.com>
2025-08-16 10:32:37 -0400

    Clean up repository: remove bridge scripts and cache files

--- send_to_automate_ai.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-resolved="$(/Users/chrislapointe/Scripts/Gemini/resolve_entities/resolve_entities.sh "$*")"
-
-ssh root@homeassistant.local "cd /share/scripts/automate_ai && ./automate_ai.sh \"$resolved\""
